d27f4e21923e302f6e9db8429a366c3f2b5fca18
Giba <giba.darocha@gmail.com>
2025-09-13 20:22:36 +0000

    added polimorfism

--- projeto/src/app/fundamentos/polimorfismo.ts
+++ projeto/src/app/fundamentos/polimorfismo.ts
@@ -1,13 +1,26 @@
+import Fusca from "@/core/fundamentos/Fusca";
 import TerminalUtil from "../util/TerminalUtil";
+import Carro from "@/core/fundamentos/Carro";
+import Ferrari from "@/core/fundamentos/Ferrari";
 
 export default async function polimorfismo() {
     TerminalUtil.titulo('Polimorfismo');
 
-    const tipoCarro = await TerminalUtil.selecao('Escolha o tipo do carro:', ['Ferrai', 'Fusca']);
+    const [tipoCarro] = await TerminalUtil.selecao('Escolha o tipo do carro:', ['Ferrai', 'Fusca']);
+    const carro: Carro = 
+        tipoCarro === 0 ? new Ferrari(): new Fusca();
     
     while (true) {
-        const continuar = await TerminalUtil.confirmacao('Deseja continuar?');
         TerminalUtil.limpar();
+        TerminalUtil.exibirChaveValor("Velocidade Máxima: ", `${carro.velocidadeMaxima} km/h`);
+        TerminalUtil.exibirChaveValor("Velocidade Atual: ", `${carro.velocidadeAtual} km/h`);
+
+        const [opcao] = await TerminalUtil.selecao("Qual Opção?", ["Acelerar", "Freiar"]);
+
+        opcao === 0 ? carro.acelerar() : carro.frear();
+        
+        const continuar = await TerminalUtil.confirmacao('Deseja continuar?');
+        
         if(!continuar) break;
     }
 
